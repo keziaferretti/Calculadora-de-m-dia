70b60dc0704f490a585bb2d67e5aebe05957a840
Kézia Ferretti <99621696+keziaferretti@users.noreply.github.com>
2022-09-11 21:43:22 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Calculadora-de-m-dia
-Calcular a media
+# Calculadora de média
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/keziaferretti/pen/WNJvGxL](https://codepen.io/keziaferretti/pen/WNJvGxL).
+
---
+++ dist/script.js
@@ -0,0 +1,28 @@
+var nome = "Kézia";
+var elementoResultado = document.getElementById("resultado");
+
+var notaDoPrimeiroBimestre = 9;
+var notaDoSegundoBimestre = 5.5;
+var notaDoTerceiroBimestre = 2;
+var notaDoQuartoBimestre = 6;
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) /4;
+
+var notaFixada = notaFinal.toFixed(1)
+
+if(notaFixada >= 7){
+  elementoResultado.innerHTML =  "Bem vindo, " + nome + "!" + "<br>" +" Sua média final é " + notaFixada + "<br>" + " Parabéns!!! você passou de ano!"
+} else {
+  elementoResultado.innerHTML = "Bem vindo, " + nome + "!" + "<br>" +" Sua média final é " + notaFixada + "<br>" + " Infelizmente você reprovou de ano!"
+}
+
+
+
+
+
+
+
+
+//toFixed ajusta as casas decimais 
+//se aluno passou ou não
+//console.log imprimir
